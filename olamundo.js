040ebc113db48c5a8636f95a0200ad3dced3629a
whoispond <anak.desouzalopes17@gmail.com>
2022-02-21 22:30:20 -0300

    primeiros passos js

---
+++ olamundo.js
@@ -0,0 +1,12 @@
+console.log ('olá mundo!')
+var a = 5;
+console.log('esse é o conteudo de a =',a)
+var b = 4;
+console.log ('esse é o resultado de',a,'+', b,'=', a+b)
+console.log ('esse é o resultado de',a,'-', b,'=', a-b)
+console.log ('esse é o resultado de',a,'x', b,'=', a*b)
+console.log ('esse é o resultado de',a,'÷', b,'=', a/b)
+
+let nome = "jessana";
+console.log ('meu nome é',nome)
+console.log (nome*b) //resulta em Not a number
